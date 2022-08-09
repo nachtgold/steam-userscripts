dd51ea2e5f049c3965c45b0bef5d4321a8712f05
Kai Zimmermann <zimmermann.k@gmail.com>
2022-08-09 21:03:08 +0200

    publisher view has same layout
    catch end of list and disable button

--- steam-curator_developer-bulk-ignore.user.js
+++ steam-curator_developer-bulk-ignore.user.js
@@ -1,11 +1,12 @@
 // ==UserScript==
-// @name         Steam developer or curator bulk ignore
+// @name         Steam developer or curator or publisher bulk ignore
 // @namespace    http://tampermonkey.net/
-// @version      1.0
+// @version      1.1
 // @description  try to take over the world!
 // @author       You
 // @match        https://store.steampowered.com/curator*
 // @match        https://store.steampowered.com/developer*
+// @match        https://store.steampowered.com/publisher*
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=steampowered.com
 // @grant        none
 // ==/UserScript==
@@ -25,27 +26,32 @@
 
     ignore_button.onclick = function(event) {
         const link = document.querySelector('.recommendation a.store_capsule:not(.ds_ignored)');
-        const recommendation = link.closest('.recommendation');
-        console.log(recommendation.querySelector('span.color_created').innerHTML);
+        if(!!link) {
+            const recommendation = link.closest('.recommendation');
+            console.log(recommendation.querySelector('span.color_created').innerHTML);
 
-        const tooltip = link.querySelector('div.ds_options');
+            const tooltip = link.querySelector('div.ds_options');
 
-        link.dispatchEvent(new Event('mouseover'));
-        link.dispatchEvent(new Event('mouseout'));
+            link.dispatchEvent(new Event('mouseover'));
+            link.dispatchEvent(new Event('mouseout'));
 
-        tooltip.click();
+            tooltip.click();
 
-        const ignore_click = () => {
-            const tooltip = document.querySelector('.ds_options_tooltip');
+            const ignore_click = () => {
+                const tooltip = document.querySelector('.ds_options_tooltip');
 
 
-            if(!!tooltip) {
-                tooltip.childElements()[1].click()
-            } else {
-                setTimeout(ignore_click, 100);
-                console.log('retry');
+                if(!!tooltip) {
+                    tooltip.childElements()[1].click()
+                } else {
+                    setTimeout(ignore_click, 100);
+                    console.log('retry');
+                }
             }
+            setTimeout(ignore_click, 100);
+        } else {
+            ignore_button.setAttribute('class', ignore_button.getAttribute('class') + " ds_ignored");
+            ignore_button.onclick = ()=>{};
         }
-        setTimeout(ignore_click, 100);
     }
 })();
