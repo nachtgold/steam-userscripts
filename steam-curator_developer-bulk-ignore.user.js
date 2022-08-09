2d452bd07e958f0545c787a674be13fc97ba4871
Kai Zimmermann <zimmermann.k@gmail.com>
2022-08-09 20:55:08 +0200

    Ignore button for developer and curator view
    similar logic like search

---
+++ steam-curator_developer-bulk-ignore.user.js
@@ -0,0 +1,51 @@
+// ==UserScript==
+// @name         Steam developer or curator bulk ignore
+// @namespace    http://tampermonkey.net/
+// @version      1.0
+// @description  try to take over the world!
+// @author       You
+// @match        https://store.steampowered.com/curator*
+// @match        https://store.steampowered.com/developer*
+// @icon         https://www.google.com/s2/favicons?sz=64&domain=steampowered.com
+// @grant        none
+// ==/UserScript==
+
+(function() {
+    'use strict';
+
+    const ignore_button_text = document.createElement('span');
+    ignore_button_text.innerHTML = 'Ignore all';
+
+    const ignore_button = document.createElement('div');
+    ignore_button.setAttribute('class', 'btnv6_blue_hoverfade btn_small');
+    ignore_button.appendChild(ignore_button_text);
+
+    const tabs = document.querySelector('.browse_tabs');
+    tabs.appendChild(ignore_button);
+
+    ignore_button.onclick = function(event) {
+        const link = document.querySelector('.recommendation a.store_capsule:not(.ds_ignored)');
+        const recommendation = link.closest('.recommendation');
+        console.log(recommendation.querySelector('span.color_created').innerHTML);
+
+        const tooltip = link.querySelector('div.ds_options');
+
+        link.dispatchEvent(new Event('mouseover'));
+        link.dispatchEvent(new Event('mouseout'));
+
+        tooltip.click();
+
+        const ignore_click = () => {
+            const tooltip = document.querySelector('.ds_options_tooltip');
+
+
+            if(!!tooltip) {
+                tooltip.childElements()[1].click()
+            } else {
+                setTimeout(ignore_click, 100);
+                console.log('retry');
+            }
+        }
+        setTimeout(ignore_click, 100);
+    }
+})();
