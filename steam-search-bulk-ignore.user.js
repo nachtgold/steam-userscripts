fb3c0507977772ccf933095e2021a46d8d80b610
Kai Zimmermann <zimmermann.k@gmail.com>
2022-08-12 22:41:30 +0200

    do nothing when nothing to do

--- steam-search-bulk-ignore.user.js
+++ steam-search-bulk-ignore.user.js
@@ -1,18 +1,16 @@
 // ==UserScript==
 // @name         Steam search bulk ignore
 // @namespace    http://tampermonkey.net/
-// @version      0.1
+// @version      1.1
 // @description  try to take over the world!
 // @author       You
-// @match        https://store.steampowered.com/search/*
+// @match        https://store.steampowered.com/search*
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=steampowered.com
 // @grant        none
 // ==/UserScript==
 
 (function() {
     'use strict';
-
-    const search_button = document.querySelector('.searchbar_left button[type="submit"]');
 
     const ignore_button_text = document.createElement('span');
     ignore_button_text.innerHTML = 'Ignore all';
@@ -24,7 +22,7 @@
 
     ignore_button.onclick = function(event) {
         // filter bundles
-        const all_hits = document.querySelectorAll('#search_resultsRows a:not(.ds_ignored')
+        const all_hits = document.querySelectorAll('#search_resultsRows a:not(.ds_ignored)')
         const bundle_hits = document.querySelectorAll('#search_resultsRows a[data-ds-bundleid]')
 
         // then go for ignore
@@ -32,22 +30,29 @@
         const search_hits = [...all_hits].filter(n => ![...bundle_hits].includes(n)).map(x => x.querySelector('div.ds_options'));
         //document.getElementsByClassName('ds_options_tooltip')[0].childElements()[1].click()
 
-        // one element in list must be hovered first
-        search_hits[0].parentElement.dispatchEvent(new Event('mouseover'));
-        search_hits[0].parentElement.dispatchEvent(new Event('mouseout'));
+        if(search_hits.length > 0) {
+            // one element in list must be hovered first
+            const link = search_hits[0].parentElement;
+            link.dispatchEvent(new Event('mouseover'));
+            link.dispatchEvent(new Event('mouseout'));
+            console.log(link.querySelector('span.title').innerHTML);
 
-        search_hits[0].click();
-        const ignore_click = () => {
-            const tooltip = document.querySelector('.ds_options_tooltip')
-            if(!!tooltip) {
-                tooltip.childElements()[1].click()
-            } else {
-                setTimeout(ignore_click, 100);
-                console.log('retry');
+            search_hits[0].click();
+            const ignore_click = () => {
+                const tooltip = document.querySelector('.ds_options_tooltip')
+                if(!!tooltip) {
+                    tooltip.childElements()[1].click()
+                } else {
+                    setTimeout(ignore_click, 100);
+                    console.log('retry');
+                }
             }
+            setTimeout(ignore_click, 100);
         }
-        setTimeout(ignore_click, 100);
     }
 
-    search_button.parentNode.insertBefore(ignore_button, search_button.nextSibling);
+    const search_button = document.querySelector('.searchbar_left button[type="submit"]');
+    if(!!search_button) {
+        search_button.parentNode.insertBefore(ignore_button, search_button.nextSibling);
+    }
 })();
